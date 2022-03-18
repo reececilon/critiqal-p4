543a372cbc66bd44c8f6ef55f1e5a7eb5b714826
reececilon <reecethat@gmail.com>
2022-03-18 13:45:20 +0000

    Create models, admin urls and allow for management of posts and comments

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Review, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_date')
+    list_display = ('title', 'slug', 'created_date', 'status', 'rating')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_date')
+    list_display = ('name', 'body', 'post', 'created_date', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- review/models.py
+++ review/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+RATING = ((1,1),(2,2),(3,3),(4,4),(5,5))
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_posts')
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    rating = models.IntegerField(choices=RATING, default=1)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
+    excerpt = models.CharField(max_length=200, blank=True)
+    yt_link = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
+    def no_of_likes(self):
+        return self.likes.count()
+
+    def get_iframe(self):
+        if self.yt_link and '=' in self.yt_link:
+            part_1 = self.yt_link.split('=', 1)[1]
+            if '&' in part_1:
+                return part_1.split('&', 1)[0]
+            else:
+                return part_1
+        else:
+            return None
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='review_comment')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f'{self.body} - {self.name}'
