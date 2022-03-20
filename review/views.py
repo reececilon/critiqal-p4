def5d79ac04188f6387d8a967409aaa3f0c17eb0
reececilon <reecethat@gmail.com>
2022-03-20 11:01:28 +0000

    Add review_content template and set up links

--- review/views.py
+++ review/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Review
 
 class ReviewList(generic.ListView):
@@ -7,3 +7,25 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1).order_by('-created_date')
     template_name = 'index.html'
     paginate_by = 8
+
+
+class ReviewInfo(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by('created_date')
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'review_content.html',
+            {
+                'review': review,
+                'comments': comments,
+                'liked': liked,
+            },
+        )
+
