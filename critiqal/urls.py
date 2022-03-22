e4b29536630e04096bdbb4aa7cc974eb4c20b4c6
reececilon <reecethat@gmail.com>
2022-03-22 13:58:11 +0000

    Add allauth authentication

--- critiqal/urls.py
+++ critiqal/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('review.urls'), name='review_urls')
+    path('', include('review.urls'), name='review_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
