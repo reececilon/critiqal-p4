10da3044e5747b72daad3ef930aef29d5317380f
reececilon <reecethat@gmail.com>
2022-04-10 20:01:37 +0000

    Deployment

--- critiqal/settings.py
+++ critiqal/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
 ALLOWED_HOSTS = ['critiqal.herokuapp.com', 'localhost']
 
 
